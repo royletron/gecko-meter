741e57090a3f7953f86fce95db99faeb992a31d4
Darren Royle <darren@easyreadsystem.com>
2016-09-16 21:21:54 +0100

    Added pre prop for chars before value

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import Meter from './components/Meter';
 export default class App extends React.Component {
   render () {
     return (
-      <Meter min={-100} max={100} value={90} />
+      <Meter pre='£' min={-100} max={100} value={90} />
     );
   }
 }
--- src/components/Meter/index.js
+++ src/components/Meter/index.js
@@ -24,17 +24,18 @@ export default class Meter extends React.Component {
     })
   }
   render () {
+    var pre = this.props.pre || '';
     return (
       <svg width='300' height='180' className={classNames(styles.this)}>
-        <text x='150' y='10' className={classNames(styles.title)}>{this.props.value}</text>
+        <text x='150' y='10' className={classNames(styles.title)}>{pre}{this.props.value}</text>
         <path transform='translate(60,55)' className={classNames(styles.arc)}
           d="M0,90 A90,90,0 1 1 180,90"
           />
         <text x='60' y= '155'>
-        {this.props.min}
+        {pre}{this.props.min}
         </text>
         <text x='240' y= '155'>
-        {this.props.max}
+        {pre}{this.props.max}
         </text>
         <g className={classNames(styles.needle)} transform='translate(150, 148)'>
           <line x1="0" y1="0" x2="0" y2="-98" className={styles.bg} x1="0" y1="0" x2="0" y2="-110" transform={`rotate(${(180 * this.state.percent) - 90} 0 0)`} />
