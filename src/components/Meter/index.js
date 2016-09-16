a997274f3e6cc72f99dffdff563a70ba35627fe6
Darren Royle <darren@easyreadsystem.com>
2016-09-16 20:10:36 +0100

    Added a nice curvey arc, yummy

--- src/components/Meter/index.js
+++ src/components/Meter/index.js
@@ -27,6 +27,9 @@ export default class Meter extends React.Component {
     return (
       <svg width='300' height='180' className={classNames(styles.this)}>
         <text x='0' y='20' className={classNames(styles.title)}>{this.props.value}</text>
+        <path transform='translate(60,55)' className={classNames(styles.arc)}
+          d="M0,90 A90,90,0 1 1 180,90"
+          />
         <text x='0' y= '40'>
         {this.props.min}-{this.props.max}
         </text>
