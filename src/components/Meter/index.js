4011be1fb8823429ab40f8b9f9542c231bfa29cd
Darren Royle <darren@easyreadsystem.com>
2016-09-16 21:20:59 +0100

    Added a needle

--- src/components/Meter/index.js
+++ src/components/Meter/index.js
@@ -36,6 +36,10 @@ export default class Meter extends React.Component {
         <text x='240' y= '155'>
         {this.props.max}
         </text>
+        <g className={classNames(styles.needle)} transform='translate(150, 148)'>
+          <line x1="0" y1="0" x2="0" y2="-98" className={styles.bg} x1="0" y1="0" x2="0" y2="-110" transform={`rotate(${(180 * this.state.percent) - 90} 0 0)`} />
+          <line x1="0" y1="0" x2="0" y2="-95" transform={`rotate(${(180 * this.state.percent) - 90} 0 0)`} />
+        </g>
       </svg>
     );
   }
