160710bee278a3eb42ac7aadc672800632257112
Darren Royle <darren@easyreadsystem.com>
2016-09-16 20:05:53 +0100

    Bit of maths and some values being passed in props

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import Meter from './components/Meter';
 export default class App extends React.Component {
   render () {
     return (
-      <Meter />
+      <Meter min={-100} max={100} value={90} />
     );
   }
 }
--- src/components/Meter/index.js
+++ src/components/Meter/index.js
@@ -4,10 +4,35 @@ import classNames from 'classnames';
 import styles from './index.css';
 
 export default class Meter extends React.Component {
+  constructor () {
+    super();
+    this.state = {
+      percent: 0
+    };
+  }
+  componentDidMount () {
+    this.calculate();
+  }
+  componentDidUpdate (prevProps, prevState) {
+    if (this.props !== prevProps) {
+      this.calculate();
+    }
+  }
+  calculate () {
+    this.setState({
+      percent: (this.props.value - this.props.min) / (this.props.max - this.props.min)
+    })
+  }
   render () {
     return (
       <svg width='300' height='180' className={classNames(styles.this)}>
-
+        <text x='0' y='20' className={classNames(styles.title)}>{this.props.value}</text>
+        <text x='0' y= '40'>
+        {this.props.min}-{this.props.max}
+        </text>
+        <text x='0' y= '60'>
+        {this.state.percent}
+        </text>
       </svg>
     );
   }
