98bfb9f1bc18d2a00bec5c324015907caf6094bf
Darren Royle <darren@easyreadsystem.com>
2016-09-17 20:34:16 +0100

    So have something that is fetching in the app instead, not sure I need to over-containerise it

--- src/App.js
+++ src/App.js
@@ -1,11 +1,55 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import { getSymbolFromCurrency } from 'currency-symbol-map';
 
-import WidgisterMeter from './containers/WidgisterMeter';
+import * as Actions from './actions/WidgisterActions';
+import Meter from './components/Meter';
 
-export default class App extends React.Component {
-  render () {
-    return (
-      <WidgisterMeter />
-    );
+const mapStateToProps = (state) => {
+  return {
+    status: state.Widgister.status,
+    widgister: state.Widgister.widgister
   }
 }
+
+const mapDispatchToProps = (dispatch) => {
+  return {
+    getNewWidgister: () => {
+      dispatch(Actions.getNewWidgister())
+    }
+  }
+}
+
+class AppComponent extends React.Component {
+  componentDidMount() {
+    this.props.getNewWidgister();
+  }
+  onNewRequest () {
+    this.props.getNewWidgister();
+  }
+  render () {
+    if (this.props.status === 'fetching') {
+      return (
+        <p>Loading ...</p>
+      )
+    } else if (this.props.status === 'idle' && this.props.widgister) {
+      var widgister = this.props.widgister;
+      console.log()
+      return (
+        <div>
+          <Meter pre={widgister.format && widgister.format === 'currency' ? getSymbolFromCurrency(widgister.unit) : undefined} min={widgister.min} max={widgister.max} value={widgister.value} />
+          <a onClick={this.onNewRequest.bind(this)}>Re-fetch</a>
+        </div>
+      );
+    } else {
+      return (
+        <div />
+      )
+    }
+  }
+}
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(AppComponent)
