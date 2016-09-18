69e375cfab5d4f60d0f556f5823466811cb2d6b1
Darren Royle <darren@easyreadsystem.com>
2016-09-18 19:59:33 +0100

    Added some more tests

--- __tests__/Meter.test.js
+++ __tests__/Meter.test.js
@@ -20,3 +20,44 @@ it('should display with prefix', () => {
   let tree = component.toJSON();
   expect(tree).toMatchSnapshot();
 })
+
+it('should report min if value is less than min', () => {
+  const component = renderer.create(
+    <Meter min={10} max={100} value={1} />
+  )
+  let tree = component.toJSON();
+  //Do shot with correct values;
+  expect(tree).toMatchSnapshot();
+  //Set value less than min
+  tree.props.value = 1;
+  //Do the shot again
+  expect(tree).toMatchSnapshot();
+})
+
+it('should report max if value is greater than max', () => {
+  const component = renderer.create(
+    <Meter min={10} max={100} value={100} />
+  )
+  let tree = component.toJSON();
+  //Do shot with correct values;
+  expect(tree).toMatchSnapshot();
+  //Set value greater than max
+  tree.props.value = 300;
+  //Do the shot again
+  expect(tree).toMatchSnapshot();
+})
+
+
+it('should flip min and max values if they are the wrong way around', () => {
+  const component = renderer.create(
+    <Meter min={10} max={100} value = {1} />
+  )
+  let tree = component.toJSON();
+  //Do shot with correct values;
+  expect(tree).toMatchSnapshot();
+  //Rearrange the same values but the wrong way around
+  tree.props.min = 100;
+  tree.props.max = 10;
+  //Do the shot again
+  expect(tree).toMatchSnapshot();
+})
